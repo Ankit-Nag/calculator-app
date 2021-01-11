f8f6408d3d1c5228c4e33e0d43441086c60e890d
Ankit-Nag <ankitknag@gmail.com>
2021-01-12 00:03:10 +0530

    update 1
    Added basic functionality

--- app.js
+++ app.js
@@ -1,15 +1,56 @@
 var output = document.querySelector(".output-screen");
 var equals = document.querySelector("#btn-equal");
 var buttons = document.querySelectorAll(".btn-small");
-
-
+var num1, num2, operator, result;
 
 function outputDisplay(input) {
-    output.innerText+=input;
-    if (input === "=") {
-        output.innerText="Output";
+    if (operator === "=") {
+        output.innerText = result;
+    } else {
+        output.innerText += input;
     }
-    else if(input === ""){}
+
+    if (input === "=") {
+        num2 = parseFloat(output.innerText);
+        console.log(num2);
+        switch (operator) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+        }
+        operator = "=";
+        output.innerText = result.toFixed(4);
+
+    } else if (input === "+") {
+        num1 = parseFloat(output.innerText);
+        console.log(num1);
+        output.innerText = "";
+        operator = "+";
+    } else if (input === "-") {
+        num1 = parseFloat(output.innerText);
+        console.log(num1);
+        output.innerText = "";
+        operator = "-";
+    } else if (input === "*") {
+        num1 = parseFloat(output.innerText);
+        console.log(num1);
+        output.innerText = "";
+        operator = "*";
+    } else if (input === "/") {
+        num1 = parseFloat(output.innerText);
+        console.log(num1);
+        output.innerText = "";
+        operator = "/";
+    }
 }
 
 buttons.forEach(item => {
